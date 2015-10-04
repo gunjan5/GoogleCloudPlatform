1af823a9a0acd3a93f2c0b6ff4a89c4bc2436f46
Gunjan Patel <patelgunjan5@gmail.com>
2015-10-03 23:52:29 -0700

    static html server

--- main.go
+++ main.go
@@ -6,10 +6,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handler)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Everything is awesome!!!"))
-}
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("Everything is awesome!!!"))
+// }
